17c0fdbadbbfb9e639b7d7c443636db20f142547
210206uuljanoo <zainidinovauuljan@gmail.com>
2021-10-17 21:07:04 +0600

    edit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws LimitException {
-	    BankAccount bankAccount =new BankAccount();
-	    bankAccount.deposit(20000);
-	    while (true){
-	        try {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(20000);
+        while (true) {
+            try {
                 bankAccount.withDraw(6000);
-                if (bankAccount.getAmount() < 6000){
-                    bankAccount.withDraw((int) bankAccount.getAmount());
+            } catch (LimitException a) {
+                try {
+                    if (bankAccount.getAmount() < 6000) {
+                        bankAccount.withDraw((int) bankAccount.getAmount());
+                    }
+                    if (bankAccount.getAmount() == 0) {
+                        break;
+                    }
+                    System.out.println(bankAccount.getAmount());
+                } catch (LimitException limitException) {
+                    limitException.printStackTrace();
                 }
-                if (bankAccount.getAmount() == 0){
-                    break;
-                }
-                System.out.println(bankAccount.getAmount());
-            }catch (LimitException limitException){
-	            limitException.printStackTrace();
+                break;
             }
+            System.out.println(bankAccount.getAmount());
         }
-        System.out.println(bankAccount.getAmount());
     }
 }
